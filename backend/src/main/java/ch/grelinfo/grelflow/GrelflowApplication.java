709a0d8757efb9d83da49ffa1cd4f49a87fea02c
grelinfo <loic.gremaud@grelinfo.ch>
2024-09-12 11:12:23 +0200

    🚀 Start backend.

---
+++ backend/src/main/java/ch/grelinfo/grelflow/GrelflowApplication.java
@@ -0,0 +1,13 @@
+package ch.grelinfo.grelflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrelflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrelflowApplication.class, args);
+	}
+
+}
