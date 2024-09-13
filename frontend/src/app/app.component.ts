0ea0401744e9ac1ae47ddba460c11561c09786a0
grelinfo <loic.gremaud@grelinfo.ch>
2024-09-13 11:35:43 +0200

    🔧 Refactor environment label handling in app component.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { SbbHeaderLeanModule } from '@sbb-esta/angular/header-lean';
 import { SbbUsermenuModule } from '@sbb-esta/angular/usermenu';
 import { SbbIconModule } from '@sbb-esta/angular/icon';
-import {SbbSidebarModule, SbbIconSidebarContainer } from '@sbb-esta/angular/sidebar';
+import { SbbSidebarModule, SbbIconSidebarContainer } from '@sbb-esta/angular/sidebar';
 import { RouterModule } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { SbbButtonModule } from '@sbb-esta/angular/button';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -20,9 +21,9 @@ import { SbbButtonModule } from '@sbb-esta/angular/button';
     SbbUsermenuModule,
     AsyncPipe,
     RouterModule,
-    RouterOutlet, 
-    SbbHeaderLeanModule, 
-    SbbUsermenuModule, 
+    RouterOutlet,
+    SbbHeaderLeanModule,
+    SbbUsermenuModule,
     SbbIconModule,
     SbbSidebarModule,
     SbbIconSidebarContainer,
@@ -32,5 +33,6 @@ import { SbbButtonModule } from '@sbb-esta/angular/button';
 })
 export class AppComponent {
   title = 'grelflow';
+  environmentLabel = environment.label;
 }
 
