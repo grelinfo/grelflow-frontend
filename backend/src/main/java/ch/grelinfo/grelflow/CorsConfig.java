2915a601422c5833555f15187583c135c7fe0c4a
grelinfo <loic.gremaud@grelinfo.ch>
2024-09-13 17:45:58 +0200

    ✨ Add CORS configuration.

---
+++ backend/src/main/java/ch/grelinfo/grelflow/CorsConfig.java
@@ -0,0 +1,34 @@
+package ch.grelinfo.grelflow;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${cors.allowed-origins:http://localhost:4200}")
+    private List<String> allowedOrigins;
+
+    @Value("${cors.allowed-methods:*}")
+    private List<String> allowedMethods;
+
+    @Value("${cors.allowed-headers:*}")
+    private List<String> allowedHeaders;
+
+    @Bean
+    CorsWebFilter corsFilter() {
+        System.out.println("allowedOrigins: " + allowedOrigins);
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(allowedOrigins);
+        config.setAllowedHeaders(allowedHeaders);
+        config.setAllowedMethods(allowedMethods);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
