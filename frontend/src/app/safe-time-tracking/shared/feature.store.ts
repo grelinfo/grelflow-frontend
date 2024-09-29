30dbca7d33b80c98cba9f5ce85a3bf7c048b7932
grelinfo <loic.gremaud@grelinfo.ch>
2024-09-29 20:22:56 +0200

    ✨ feat: Update feature list component UI and functionality

--- frontend/src/app/safe-time-tracking/shared/feature.store.ts
+++ frontend/src/app/safe-time-tracking/shared/feature.store.ts
@@ -1,25 +1,85 @@
 import { patchState, signalStore, withMethods } from '@ngrx/signals';
 import { Feature } from './feature.model';
-import { addEntity, removeEntity, withEntities, setEntity } from  '@ngrx/signals/entities';
+import {
+  addEntity,
+  removeEntity,
+  withEntities,
+  setEntity,
+  removeAllEntities,
+} from '@ngrx/signals/entities';
 import { withStorageSync } from '@angular-architects/ngrx-toolkit';
+import { FeatureService } from './feature.service';
+import { inject } from '@angular/core';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import {
+  pipe,
+  switchMap,
+  tap,
+  filter,
+  mergeMap,
+  exhaustMap,
+  debounceTime,
+  concatMap
+} from 'rxjs';
+import { tapResponse } from '@ngrx/operators';
 
 export const FeatureStore = signalStore(
-    withEntities<Feature>(),
-    withMethods((store) => ({
-        add(feature: Feature): void {
-            patchState(store, addEntity(feature));
-        },
-        set(feature: Feature): void {
-            patchState(store, setEntity(feature))
-        },
-        remove(id: string): void {
-            patchState(store, removeEntity(id));
-        },
-    })),
-    withStorageSync({
-        key: 'synced',
-        autoSync: true,
-        storage: () => localStorage,
-    })
+  withEntities<Feature>(),
+  withMethods((store, featureService = inject(FeatureService)) => ({
+    load: rxMethod<string>(
+      pipe(
+        // Skip if the feature is already in the store
+        filter((id) => !!id && !store.entityMap()[id]),
+        concatMap((id) =>
+          featureService.fetch(id).pipe(
+            tapResponse(
+              (feature) => patchState(store, addEntity(feature)),
+              (error) => console.error(error)
+            )
+          )
+        )
+      )
+    ),
+    refresh: rxMethod<string>(
+      pipe(
+        debounceTime(1000),
+        // Skip if the feature is not in the store
+        filter((id) => !!id && !!store.entityMap()[id]),
+        concatMap((id) =>
+          featureService.fetch(id).pipe(
+            tapResponse(
+              (feature) => patchState(store, setEntity(feature)),
+              (error) => console.error(error)
+            )
+          )
+        )
+      )
+    ),
+    refreshAll: rxMethod<void>(
+      pipe(
+        debounceTime(1000),
+        exhaustMap(() => store.entities()),
+        mergeMap((feature) =>
+          featureService.fetch(feature.id).pipe(
+            tapResponse(
+              (fetchedFeature) => patchState(store, setEntity(fetchedFeature)),
+              (error) => console.error(error)
+            )
+          )
+        )
+      )
+    ),
+    remove: rxMethod<string>(
+      // Remove the feature from the store
+      tap((id) => patchState(store, removeEntity(id)))
+    ),
+    removeAll: rxMethod<void>(
+      pipe(tap(() => patchState(store, removeAllEntities())))
+    ),
+  })),
+  withStorageSync({
+    key: 'safe-time-tracking-feature-store',
+    autoSync: true,
+    storage: () => localStorage,
+  })
 );
-
